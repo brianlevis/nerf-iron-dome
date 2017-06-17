6c0f9c6179c3a128fb5d5e693054595523e62805
brianlevis <brianlevis1@gmail.com>
2017-06-17 13:41:03 -0700

    added auto mode to controller function

--- arduino/GunController/GunController.ino
+++ arduino/GunController/GunController.ino
@@ -1,16 +1,16 @@
 /* Initial Gun Test
 
-  The circuit:
-   Pusher switch on pin 4
-   Acceleration motor relay input on pin 7
-   Firing motor relay input on pin 8
-   Manual input on pin 12
+The circuit:
+Pusher switch on pin 4
+Acceleration motor relay input on pin 7
+Firing motor relay input on pin 8
+Manual input on pin 12
 
 */
 
 #include <Servo.h>
 
-#define REV_UP_TIME    200
+#define REV_UP_TIME    300
 #define MAX_TILT_UP   2000
 #define MAX_TILT_DOWN 1300
 #define MAX_PAN_LEFT   800
@@ -28,11 +28,15 @@ const int ledPin               = 13;
 
 const char startCode = 's';
 const char waitCode = 'w';
+const char revCode = 'r';
 const char fireCode = 'f';
 const char tiltCode = 't';
 const char panCode =  'p';
 const char endCode = 'e';
 const char errorCode = 'x';
+
+const int startAutoFireCode = 65535;
+const int stopAutoFireCode = 0;
 
 unsigned long startTime;
 int serialAction;
@@ -43,131 +47,157 @@ Servo panServo;
 Servo tiltServo;
 
 bool withinRange(int var, int low, int high) {
-  return low <= var && var <= high;
+    return low <= var && var <= high;
 }
 
 void revUp() {
-  digitalWrite(accelerationMotorPin, LOW);
+    digitalWrite(accelerationMotorPin, LOW);
 }
 
 void revDown() {
-  digitalWrite(accelerationMotorPin, HIGH);
+    digitalWrite(accelerationMotorPin, HIGH);
 }
 
 void pusherOn() {
-  digitalWrite(pusherMotorPin, LOW);
+    digitalWrite(pusherMotorPin, LOW);
 }
 
 void pusherOff() {
-  digitalWrite(pusherMotorPin, HIGH);
+    digitalWrite(pusherMotorPin, HIGH);
 }
 
 void pulsePusher() {
-  pusherOn();
-  delay(15);
-  pusherOff();
-  delay(80);
+    pusherOn();
+    delay(15);
+    pusherOff();
+    delay(80);
+}
+
+void resetPusher() {
+    pusherOff();
+    while (digitalRead(pusherSwitchPin) == HIGH) pulsePusher();
 }
 
 void pan(int location) {
-  if (withinRange(location, MAX_PAN_LEFT, MAX_PAN_RIGHT))
-    panServo.writeMicroseconds(location);
-  else
-    Serial.println("Pan location out of range!");
+    if (withinRange(location, MAX_PAN_LEFT, MAX_PAN_RIGHT)) {
+        panServo.writeMicroseconds(location);
+    }
+    else {
+        Serial.println("Pan location out of range!");
+    }
 }
 
 void tilt(int location) {
-  if (withinRange(location, MAX_TILT_DOWN, MAX_TILT_UP))
-    tiltServo.writeMicroseconds(location);
-  else
-    Serial.println("Tilt location out of range!");
+    if (withinRange(location, MAX_TILT_DOWN, MAX_TILT_UP)) {
+        tiltServo.writeMicroseconds(location);
+    } else {
+        Serial.println("Tilt location out of range!");
+    }
 }
 
-void fire(int numShots) {
-  if (!withinRange(numShots, 0, 37)) {
-    Serial.println("Inproper number of shots received!");
-    return;
-  }
-  startTime = millis();
-  revUp();
-  while (millis() - startTime < REV_UP_TIME);
-  for ( ; numShots > 0; numShots--) {
-    while (digitalRead(pusherSwitchPin) == LOW) pulsePusher();
-    while (digitalRead(pusherSwitchPin) == HIGH) pulsePusher();
-  }
-  revDown();
+void rev(int argument) {
+    if (argument == 0) {
+        revDown();
+    } else if (argument == 65535) {
+        revUp();
+    } else {
+        Serial.println("Bad rev argument!");
+    }
+}
+
+void fire(int argument) {
+    if (withinRange(numShots, 1, 37)) {
+        fireShots(argument)
+    } else if (argument == 0) {
+        resetPusher();
+    } else if (argument == 65535) {
+        pusherOn();
+    } else {
+        Serial.println("Inproper number of shots received!");
+    }
+}
+
+void fireShots(int numShots) {
+    startTime = millis();
+    revUp();
+    while (millis() - startTime < REV_UP_TIME);
+    for ( ; numShots > 0; numShots--) {
+        while (digitalRead(pusherSwitchPin) == LOW) pulsePusher();
+        while (digitalRead(pusherSwitchPin) == HIGH) pulsePusher();
+    }
+    revDown();
 }
 
 byte getNextByte() {
-  do {
-    serialAction = Serial.read();
-  } while (serialAction == -1);
-  return byte(serialAction);
+    do {
+        serialAction = Serial.read();
+    } while (serialAction == -1);
+    return byte(serialAction);
 }
 
 void waitForInput() {
-  Serial.write('w');
-  byte start = getNextByte();
-  if (start != startCode) {
-    Serial.println("Invalid start byte");
-    return;
-  }
-  byte actionCode = getNextByte();
-  Serial.write(actionCode);
-  int parameter = (int(getNextByte()) << 4) + int(getNextByte());
-  Serial.println(parameter);
-  byte end = getNextByte();
-  if (end != endCode) {
-    Serial.println("Invalid start byte");
-    return;
-  }
-  switch (actionCode) {
-    case fireCode:
-      fire(parameter);
-      break;
-    case tiltCode:
-      tilt(parameter);
-      break;
-    case panCode:
-      pan(parameter);
-      break;
-    default:
-      Serial.println("Invalid action code");
-  }
+    Serial.write('w');
+    byte start = getNextByte();
+    if (start != startCode) {
+        Serial.println("Invalid start byte");
+        return;
+    }
+    byte actionCode = getNextByte();
+    // Serial.write(actionCode);
+    int parameter = (int(getNextByte()) << 8) + int(getNextByte());
+    // Serial.println(parameter);
+    byte end = getNextByte();
+    if (end != endCode) {
+        Serial.println("Invalid start byte");
+        return;
+    }
+    switch (actionCode) {
+        case fireCode:
+            fire(parameter);
+            break;
+        case tiltCode:
+            tilt(parameter);
+            break;
+        case panCode:
+            pan(parameter);
+            break;
+        case revCode:
+            rev(parameter);
+            break;
+        default:
+            Serial.println("Invalid action code");
+    }
 }
 
-void verifyComms() {
-  do {
-    Serial.println("Waiting for a verification message...");
-    do {
-      serialAction = Serial.read();
-    } while (serialAction == -1);
-    Serial.println("Please send 1500...");
-    do {
-      s = Serial.readString();
-      serialParameter = s.toInt();
-    } while (serialParameter == 0);
-  } while (serialAction != 'v' || serialParameter != 1500);
-}
+// void verifyComms() {
+//     do {
+//         Serial.println("Waiting for a verification message...");
+//         do {
+//             serialAction = Serial.read();
+//         } while (serialAction == -1);
+//         Serial.println("Please send 1500...");
+//         do {
+//             s = Serial.readString();
+//             serialParameter = s.toInt();
+//         } while (serialParameter == 0);
+//     } while (serialAction != 'v' || serialParameter != 1500);
+// }
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(pusherSwitchPin, INPUT_PULLUP);
-  pinMode(accelerationMotorPin, OUTPUT);
-  pinMode(pusherMotorPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(ledPin, OUTPUT);
-  panServo.attach(panServoPin);
-  tiltServo.attach(tiltServoPin);
-  pan(PAN_MIDPOINT);
-  tilt(TILT_MIDPOINT);  
-  revDown();
-  pusherOff();
-//  verifyComms();
-  while (digitalRead(pusherSwitchPin) == HIGH) pulsePusher();
+    Serial.begin(9600);
+    pinMode(pusherSwitchPin, INPUT_PULLUP);
+    pinMode(accelerationMotorPin, OUTPUT);
+    pinMode(pusherMotorPin, OUTPUT);
+    pinMode(buttonPin, INPUT_PULLUP);
+    pinMode(ledPin, OUTPUT);
+    panServo.attach(panServoPin);
+    tiltServo.attach(tiltServoPin);
+    pan(PAN_MIDPOINT);
+    tilt(TILT_MIDPOINT);
+    revDown();
+    resetPusher();
 }
 
 void loop() {
-  waitForInput();
+    waitForInput();
 }
-
--- stable_controller_operation.py
+++ stable_controller_operation.py
@@ -3,10 +3,14 @@ sys.path.append('/home/pi/Xbox')
 
 import xbox
 import serial
+import subprocess
 from time import time
 
 joy = xbox.Joystick()
 ser = serial.Serial('/dev/ttyACM0')
+
+semiauto_cmd = ["espeak", "-vlv+m3", "-s300", "'Semiautomatik'"]
+auto_cmd = ["espeak", "-vlv+m3", "-s300", "'Automatik'"]
 
 # espeak -vlv+m3 -s300 "Semiautomatik"
 # espeak -vlv+m3 -s300 "Automatik"
@@ -20,6 +24,7 @@ ser = serial.Serial('/dev/ttyACM0')
 
 x = 0
 y = 0
+auto = False
 
 def pan(val):
     return 1455 + val * 745
@@ -31,15 +36,39 @@ def send_command(action_code, argument):
     argument=int(argument)
     print('sending %s %d'%(action_code,argument))
     ser.write(bytes([115, ord(action_code), (argument & 0b1111111100000000) >> 8, argument & 0b11111111, 101]))
-    return
 
+time0 = time()
 time1 = time()
 time2 = time()
 time3 = time()
+auto_firing = False
+auto_revving = False
 while not joy.Back():
-    if joy.rightTrigger() > .5 and time() > time1 + 1.5:
-        send_command('f', 1)
-        time1 = time()
+    if auto:
+        if joy.rightTrigger() > .5 and auto_firing = False:
+            send_command('f', 65535)
+            auto_firing = True
+        elif joy.rightTrigger() < .5 and and auto_firing = True:
+            send_command('f', 0)
+            auto_firing = False
+        if joy.leftTrigger() > .5 and auto_revving = False:
+            send_command('r', 65535)
+            auto_revving = True
+        elif joy.leftTrigger() < .5 and auto_revving = True:
+            send_command('r', 0)
+            auto_revving = False
+        if joy.X() and time() > time0 + 1:
+            subprocess.call(semiauto_cmd)
+            time0 = time()
+            auto = False
+    else:
+        if joy.rightTrigger() > .5 and time() > time1 + 1.5:
+            send_command('f', 1)
+            time1 = time()
+        if joy.X() and time() > time0 + 1:
+            subprocess.call(auto_cmd)
+            time0 = time()
+            auto = True
     x_p, y_p = joy.leftStick()
     if time() > time2 + .1:
         x += x_p / 10
