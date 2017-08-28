3c32ea9a5c1bd6357a61f2b173e429667b2b6dcc
brianlevis <brianlevis1@gmail.com>
2017-08-27 19:13:24 -0700

    add code for new serial protocol and panning

--- arduino/GunController/GunController.ino
+++ arduino/GunController/GunController.ino
@@ -10,14 +10,17 @@ Manual input on pin 12
 
 #include <Servo.h>
 
-#define REV_UP_TIME    300
-#define MAX_FIRE_TIME 1000
-#define MAX_TILT_UP   2000
-#define MAX_TILT_DOWN 1300
-#define MAX_PAN_LEFT   800
-#define MAX_PAN_RIGHT 2200
-#define TILT_MIDPOINT 1500
-#define PAN_MIDPOINT  1455
+#define REV_UP_TIME      300
+#define MAX_FIRE_TIME   1000
+#define MAX_TILT_UP     2000
+#define MAX_TILT_DOWN   1300
+#define MAX_PAN_LEFT     800
+#define MAX_PAN_RIGHT   2200
+#define TILT_MIDPOINT   1500
+#define PAN_MIDPOINT    1455
+
+#define ACCELERATION       0.2
+#define UPDATE_INTERVAL 2000
 
 const int pusherSwitchPin      =  4;
 const int accelerationMotorPin =  7;
@@ -27,32 +30,50 @@ const int panServoPin          = 11;
 const int buttonPin            = 12;
 const int ledPin               = 13;
 
-const char startCode   = 's';
-const char waitCode    = 'w';
-const char revCode     = 'r';
-const char fireCode    = 'f';
-const char tiltCode    = 't';
-const char panCode     = 'p';
-const char endCode     = 'e';
-const char invalidCode = 'i';
-const char errorCode   = 'x';
-const char killCode    = 'k';
+const char waitCode      = 'w';
+const char errorCode     = 'x';
+const char invalidCode   = 'i';
+
+const char startCode     = 's';
+const char endCode       = 'e';
+const char revCode       = 'r';
+const char fireCode      = 'f';
+const char tiltCode      = 't';
+const char panCode       = 'p';
+const char velocityCode  = 'v';
+const char heartbeatCode = 'h';
+const char killCode      = 'k';
+
+const int commandInputCount = 7;
+char validInputs[commandInputCount] = {
+    revCode, fireCode, tiltCode, panCode, velocityCode, heartbeatCode, killCode
+};
 
 const int startAutoFireCode = 65535;
 const int stopAutoFireCode = 0;
 
-unsigned long startTime;
+// state variables
+int panState         = PAN_MIDPOINT;
+int panGoal          = panState;
+int panHalfway       = panState;
+float panVelocity    = 0.0;
+int tiltGoal         = TILT_MIDPOINT;
+int tiltState        = TILT_MIDPOINT;
+int tiltVelocity     = 0;
+int tiltAcceleration = 0;
+
+bool velocityMode = false;
+
+long lastUpdateTime = micros();
+
+long timeElapsedSinceUpdate;
 int serialAction;
 unsigned int serialParameter;
 String s;
+int i;
 // PWM range 556-2420 => 1488
 Servo panServo;
 Servo tiltServo;
-
-void reportError(char errorMessage[]) {
-    Serial.write(errorCode);
-    Serial.println(errorMessage);
-}
 
 bool withinRange(int var, int low, int high) {
     return low <= var && var <= high;
@@ -86,59 +107,103 @@ void resetPusher() {
     while (digitalRead(pusherSwitchPin) == HIGH) pulsePusher();
 }
 
+/*
+---------------------------------------------------------
+                        Movement
+*/
+
+void setPanLocation(int argument) {
+    // argument: [-700, 700]
+    panGoal = PAN_MIDPOINT + argument;
+}
+
+void setTiltLocation(int argument) {
+    // argument: [-300, 500]
+    tiltGoal = TILT_MIDPOINT + argument;
+}
+
+void updateState() {
+
+}
+
 void pan(int location) {
     if (withinRange(location, MAX_PAN_LEFT, MAX_PAN_RIGHT)) {
         panServo.writeMicroseconds(location);
-    }
-    else {
-        reportError("Pan location out of range!");
-    }
+    }// else {
+    //     reportError("Pan location out of range!");
+    // }
 }
 
 void tilt(int location) {
     if (withinRange(location, MAX_TILT_DOWN, MAX_TILT_UP)) {
         tiltServo.writeMicroseconds(location);
-    } else {
-        reportError("Tilt location out of range!");
-    }
+    }// else {
+    //     reportError("Tilt location out of range!");
+    // }
 }
 
-void rev(int argument) {
-    if (argument == 0) {
-        revDown();
-    } else if (argument == 65535) {
-        revUp();
-    } else {
-        reportError("Bad rev argument!");
-    }
+/*
+---------------------------------------------------------
+                         Firing
+*/
+
+//void rev(int argument) {
+//    if (argument == 0) {
+//        revDown();
+//    } else if (argument == 65535) {
+//        revUp();
+//    } else {
+//        reportError("Bad rev argument!");
+//    }
+//}
+//
+//void fireShots(int numShots) {
+//    startTime = millis();
+//    revUp();
+//    while (millis() - startTime < REV_UP_TIME);
+//    for ( ; numShots > 0; numShots--) {
+//        startTime = millis();
+//        while (digitalRead(pusherSwitchPin) == LOW && millis() - startTime < MAX_FIRE_TIME) pulsePusher();
+//        while (digitalRead(pusherSwitchPin) == HIGH && millis() - startTime < MAX_FIRE_TIME) pulsePusher();
+//        if (millis() - startTime > MAX_FIRE_TIME) {
+//            revDown();
+//            reportError("Firing sensor not responding!");
+//        }
+//    }
+//    revDown();
+//}
+//
+//void fire(int argument) {
+//    if (withinRange(argument, 1, 37)) {
+//        fireShots(argument);
+//    } else if (argument == 0) {
+//        resetPusher();
+//    } else if (argument == 65535) {
+//        pusherOn();
+//    } else {
+//        reportError("Inproper number of shots received!");
+//    }
+//}
+
+/*
+---------------------------------------------------------
+                Serial Communication
+*/
+
+void requestInput() {
+    Serial.write(waitCode);
 }
 
-void fireShots(int numShots) {
-    startTime = millis();
-    revUp();
-    while (millis() - startTime < REV_UP_TIME);
-    for ( ; numShots > 0; numShots--) {
-        startTime = millis();
-        while (digitalRead(pusherSwitchPin) == LOW && millis() - startTime < MAX_FIRE_TIME) pulsePusher();
-        while (digitalRead(pusherSwitchPin) == HIGH && millis() - startTime < MAX_FIRE_TIME) pulsePusher();
-        if (millis() - startTime > MAX_FIRE_TIME) {
-            revDown();
-            reportError("Firing sensor not responding!");
-        }
-    }
-    revDown();
+void reportInvalidInput(char message[]) {
+    Serial.write(invalidCode);
+    Serial.println(message);
 }
 
-void fire(int argument) {
-    if (withinRange(argument, 1, 37)) {
-        fireShots(argument);
-    } else if (argument == 0) {
-        resetPusher();
-    } else if (argument == 65535) {
-        pusherOn();
-    } else {
-        reportError("Inproper number of shots received!");
+bool isValidInput(char operationCode) {
+    for (i = 0; i < commandInputCount; i++) {
+        if (operationCode == validInputs[i]) return true;
     }
+    return false;
 }
 
 byte getNextByte() {
@@ -148,53 +213,65 @@ byte getNextByte() {
     return byte(serialAction);
 }
 
-void waitForInput() {
-    Serial.write(waitCode);
-    byte start = getNextByte();
-    if (start != startCode) {
-        reportError("Invalid start byte");
-        return;
-    }
-    byte actionCode = getNextByte();
-    // Serial.write(actionCode);
-    int parameter = (int(getNextByte()) << 8) + int(getNextByte());
-    // Serial.println(parameter);
-    byte end = getNextByte();
-    if (end != endCode) {
-        reportError("Invalid start byte");
-        return;
-    }
-    switch (actionCode) {
-        case fireCode:
-            fire(parameter);
+void processInput() {
+    while (Serial.available() >= 5) {
+        if (Serial.read() != startCode) {
+            reportInvalidInput("Expected start byte");
             break;
-        case tiltCode:
-            tilt(parameter);
+        }
+        char operationCode = Serial.read();
+        if (!isValidInput(operationCode)) {
+            reportInvalidInput("Invalid operation code");
             break;
-        case panCode:
-            pan(parameter);
+        }
+
+        byte byte0 = Serial.read();
+        byte byte1 = Serial.read();
+
+        if (Serial.read() != endCode) {
+            reportInvalidInput("Expected end byte");
             break;
-        case revCode:
-            rev(parameter);
-            break;
-        default:
-            reportError("Invalid action code");
+        }
+
+        switch (operationCode) {
+            case revCode:
+                {
+                    // [0, 255]
+                    uint8_t speed = byte0;
+                    uint8_t timeout = byte1;
+                }
+                break;
+            case fireCode:
+                {
+                    // [0, 255]
+                    uint8_t shotCount = byte0;
+                    uint8_t shootingFrequency = byte1;
+                }
+                break;
+            case tiltCode:
+                setTiltLocation((int(byte0) << 8) + int(byte1));
+                break;
+            case panCode:
+                setPanLocation((int(byte0) << 8) + int(byte1));
+                break;
+            case velocityCode:
+                // [-128, 127]
+//                int8_t panVelocity = byte0;
+//                int8_t tiltVelocity = byte1;
+                break;
+            case heartbeatCode:
+                break;
+            case killCode:
+                break;
+        }
+
+        requestInput();
     }
 }
 
-// void verifyComms() {
-//     do {
-//         Serial.println("Waiting for a verification message...");
-//         do {
-//             serialAction = Serial.read();
-//         } while (serialAction == -1);
-//         Serial.println("Please send 1500...");
-//         do {
-//             s = Serial.readString();
-//             serialParameter = s.toInt();
-//         } while (serialParameter == 0);
-//     } while (serialAction != 'v' || serialParameter != 1500);
-// }
+/*
+---------------------------------------------------------
+*/
 
 void setup() {
     Serial.begin(9600);
@@ -209,8 +286,24 @@ void setup() {
     tilt(TILT_MIDPOINT);
     revDown();
     resetPusher();
+    requestInput();
 }
 
 void loop() {
-    waitForInput();
+    processInput();
+
+    timeElapsedSinceUpdate = micros() - lastUpdateTime;
+    if (panState != panGoal && timeElapsedSinceUpdate > UPDATE_INTERVAL) {
+        lastUpdateTime = micros();
+        panState += int(panVelocity);
+        if ((panHalfway < panGoal && panState < panHalfway) || (panHalfway > panGoal && panState > panHalfway)) {
+            panVelocity += ACCELERATION * timeElapsedSinceUpdate / 1000;
+        } else {
+            panVelocity -= ACCELERATION * timeElapsedSinceUpdate / 1000;
+        }
+        if ((panHalfway < panGoal && panState > panGoal) || (panHalfway > panGoal && panState < panGoal)) {
+            panState = panGoal;
+        }
+        pan(panGoal);
+    }
 }
