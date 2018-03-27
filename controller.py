edae0598ea07c3d20b6ea794aa45c2564baf31ac
brianlevis <brianlevis1@gmail.com>
2018-03-26 19:16:42 -0700

    update xbox controller code

--- controller.py
+++ controller.py
@@ -34,69 +34,20 @@ panCode   = 'p'
 endCode   = 'e'
 errorCode = 'x'
 
-x = 0
-y = 0
-auto = False
+times = [time()] * 4
+elapsed = [0] * len(times)
 
-def pan(val):
-    return 1455 + val * 745
-
-def tilt(val):
-    return 1650 + val * 350
-
-def send_command(action_code, argument):
-    status = serial.read()
-    if status == 'x':
-        print('ERROR:', serial.readline())
-        exit(1)
-    elif status != 'w':
-        print('Incorrect message prefix:', status)
-        exit(1)
-    argument=int(argument)
-    print('sending %s %d'%(action_code,argument))
-    ser.write(bytes([115, ord(action_code), (argument & 0b1111111100000000) >> 8, argument & 0b11111111, 101]))
-
-time0 = time()
-time1 = time()
-time2 = time()
-time3 = time()
-auto_firing = False
-auto_revving = False
 while not joy.Back():
-    if auto:
-        if joy.rightTrigger() > .5 and not auto_firing and auto_revving:
-            send_command('f', 65535)
-            auto_firing = True
-        elif joy.rightTrigger() < .5 and auto_firing:
-            send_command('f', 0)
-            auto_firing = False
-        if joy.leftTrigger() > .5 and not auto_revving:
-            send_command('r', 65535)
-            auto_revving = True
-        elif joy.leftTrigger() < .5 and auto_revving:
-            send_command('r', 0)
-            auto_revving = False
-        if joy.X() and time() > time0 + 1 and not auto_firing and not auto_revving:
-            subprocess.call(semiauto_cmd)
-            time0 = time()
-            auto = False
-    else:
-        if joy.rightTrigger() > .5 and time() > time1 + 1.5:
-            send_command('f', 1)
-            time1 = time()
-        if joy.X() and time() > time0 + 1:
-            subprocess.call(auto_cmd)
-            time0 = time()
-            auto = True
-    x_p, y_p = joy.leftStick()
-    if time() > time2 + .1:
-        x += x_p / 10
-        x = max(x, -1); x = min(x, 1)
-        send_command('p', pan(x))
-        time2 = time()
-    if time() > time3 + .1:
-        y += y_p / 10
-        y = max(y, -1); y = min(y, 1)
-        send_command('t', pan(y))
-        time3 = time()
+    c_t = time()
+    elapsed = [c_t - t for t in times]
+    if elapsed[0] > 0.4 and joy.rightTrigger() > .5:
+        nerf_turret.fire(1)
+        times[0] = time()
+    if elapsed[1] > 0.3:
+        nerf_turret.rev(max(0, joy.leftTrigger() - 0.3) / 0.7 * 75)
+        times[1] = time()
+    if elapsed[2] > 0.2:
+        x_p, y_p = joy.rightStick()
+        nerf_turret.set_velocity(x_p * 75, y_p * 75)
+        times[2] = time()
 joy.close()
--- nerf_turret.py
+++ nerf_turret.py
@@ -53,6 +53,7 @@ def send_command(action_code, argument):
     print('sending %s %d'%(action_code, argument))
     ser.write(bytes([115, ord(action_code), (argument & 0xff00) >> 8, argument & 0xff, 101]))
 
+# [0, 127], [0, 127]
 def set_velocity(pan_velocity, tilt_velocity):
     pan_velocity, tilt_velocity = pan_velocity & 0xff, tilt_velocity & 0xff # truncate to byte size
     arg = (pan_velocity << 8) + tilt_velocity
@@ -62,6 +63,7 @@ def move(pan_location, tilt_location):
     send_command('p', pan_location)
     send_command('t', tilt_location)
 
+# [0, 255]
 def rev(rev_speed):
     send_command('r', rev_speed << 8)
 
@@ -88,14 +90,14 @@ def tilt(position):
     else:
         send_command(tiltCode, position)
 
-def velocity(pan, tilt):
-    if -128 > pan or pan > 127:
-        print("Pan velocity %s out of range [-127, 128]", str(pan))
-    elif type(pan) != int:
-        print("Pan velocity must be of type int, not %s", str(type(pan)))
-    if -128 > tilt or tilt > 127:
-        print("Tilt velocity %s out of range [-127, 128]", str(tilt))
-    elif type(tilt) != int:
-        print("Tilt velocity must be of type int, not %s", str(type(tilt)))
-    else:
-        send_command(velocityCode, ((0xff & pan) << 8) + (0xff & tilt))
+# def velocity(pan, tilt):
+#     if -128 > pan or pan > 127:
+#         print("Pan velocity %s out of range [-127, 128]", str(pan))
+#     elif type(pan) != int:
+#         print("Pan velocity must be of type int, not %s", str(type(pan)))
+#     if -128 > tilt or tilt > 127:
+#         print("Tilt velocity %s out of range [-127, 128]", str(tilt))
+#     elif type(tilt) != int:
+#         print("Tilt velocity must be of type int, not %s", str(type(tilt)))
+#     else:
+#         send_command(velocityCode, ((0xff & pan) << 8) + (0xff & tilt))
