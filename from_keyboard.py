73fb29e8bbb5ffa60030a8c25bb439a6f317db80
brianlevis <brianlevis1@gmail.com>
2018-03-26 18:29:07 -0700

    update api calls

--- from_keyboard.py
+++ from_keyboard.py
@@ -17,9 +17,7 @@ def main_loop(rel):
     vel = 60
     try:
         while True:
-
             char = screen.getch()
-
             if char == ord('q'):
                 break
             elif char == ord('u'):
@@ -40,34 +38,29 @@ def main_loop(rel):
             elif char == curses.KEY_RIGHT:
                 last_pressed = time.time()
                 if motion != 'r':
-                    nerf_turret.send_v(rel*vel, 0)
+                    nerf_turret.set_velocity(rel*vel, 0)
                     motion = 'r'
-
             elif char == curses.KEY_LEFT:
                 last_pressed = time.time()
                 if motion != 'l':
-                    nerf_turret.send_v(rel*-vel, 0)
+                    nerf_turret.set_velocity(rel*-vel, 0)
                     motion = 'l'
-
             elif char == curses.KEY_UP:
                 last_pressed = time.time()
                 if motion != 'u':
-                    nerf_turret.send_v(0, max(vel-20,0))
+                    nerf_turret.set_velocity(0, max(vel-20,0))
                     motion = 'u'
-
             elif char == curses.KEY_DOWN:
                 last_pressed = time.time()
                 if motion != 'd':
-                    nerf_turret.send_v(0, min(-vel+20,0))
+                    nerf_turret.set_velocity(0, min(-vel+20,0))
                     motion = 'd'
-
             if motion != 'n' and time.time() - last_pressed > 0.5:
-                nerf_turret.send_v(0,0)
+                nerf_turret.set_velocity(0,0)
                 motion = 'n'
-
     finally:
-        nerf_turret.send_command('p', 0)
-        nerf_turret.send_command('t', 0)
+        nerf_turret.pan(0)
+        nerf_turret.tilt(0)
         curses.nocbreak(); screen.keypad(0); curses.echo(); screen.nodelay(0)
         curses.endwin()
 
--- nerf_turret.py
+++ nerf_turret.py
@@ -24,16 +24,6 @@ velocityCode  = 'v'
 heartbeatCode = 'h'
 killCode      = 'k'
 
-# x = 0
-# y = 0
-# auto = False
-#
-# def pan(val):
-#     return 1455 + val * 745
-#
-# def tilt(val):
-#     return 1650 + val * 350
-
 # Example usage:
 # set_velocity(10, 10); sleep(1);
 # set_velocity(-20,-20); sleep(1);
@@ -109,48 +99,3 @@ def velocity(pan, tilt):
         print("Tilt velocity must be of type int, not %s", str(type(tilt)))
     else:
         send_command(velocityCode, ((0xff & pan) << 8) + (0xff & tilt))
-
-# time0 = time()
-# time1 = time()
-# time2 = time()
-# time3 = time()
-# auto_firing = False
-# auto_revving = False
-# while not joy.Back():
-#     if auto:
-#         if joy.rightTrigger() > .5 and not auto_firing and auto_revving:
-#             send_command('f', 65535)
-#             auto_firing = True
-#         elif joy.rightTrigger() < .5 and auto_firing:
-#             send_command('f', 0)
-#             auto_firing = False
-#         if joy.leftTrigger() > .5 and not auto_revving:
-#             send_command('r', 65535)
-#             auto_revving = True
-#         elif joy.leftTrigger() < .5 and auto_revving:
-#             send_command('r', 0)
-#             auto_revving = False
-#         if joy.X() and time() > time0 + 1 and not auto_firing and not auto_revving:
-#             subprocess.call(semiauto_cmd)
-#             time0 = time()
-#             auto = False
-#     else:
-#         if joy.rightTrigger() > .5 and time() > time1 + 1.5:
-#             send_command('f', 1)
-#             time1 = time()
-#         if joy.X() and time() > time0 + 1:
-#             subprocess.call(auto_cmd)
-#             time0 = time()
-#             auto = True
-#     x_p, y_p = joy.leftStick()
-#     if time() > time2 + .1:
-#         x += x_p / 10
-#         x = max(x, -1); x = min(x, 1)
-#         send_command('p', pan(x))
-#         time2 = time()
-#     if time() > time3 + .1:
-#         y += y_p / 10
-#         y = max(y, -1); y = min(y, 1)
-#         send_command('t', pan(y))
-#         time3 = time()
-# joy.close()
