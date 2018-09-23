2d5106dae433209fea663196691dab6eca634c4f
Brian Levis <brianlevis1@gmail.com>
2018-09-22 21:23:42 -0700

    tweaks

--- face_detect.py
+++ face_detect.py
@@ -31,10 +31,13 @@ print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 time_since_face = time.time()
+revving = False
 # loop over the frames from the video stream
 while True:
-    if time_since_face > 5:
+    if time.time() - time_since_face > 3 and revving:
+        print("Haven't seen a face in a while...")
         nerf_turret.rev(0)
+        revving = False
     frame = vs.read()
     frame = cv2.resize(frame, (400, 300))
     frame = cv2.flip(frame, -1)
@@ -50,33 +53,34 @@ while True:
     )
     if len(faces) == 0:
         continue
+    time_since_face = time.time()
     print("Found {} faces".format(len(faces)))
     (fx, fy, fw, fh) = faces[0]
     x, y = int(round(fx + fw / 2)), int(round(fy + fh / 2))
     print("Face at", (x, y))
     # Center: (216, 157)
-    dx, dy = x-216, 130-y
-    set_change(dx, dy)
+    dx, dy = x-216, 100-y # 5 for gravity
+    set_change(int(math.ceil(dx*8/10)), int(dy*8/10))
     # dx, dy = 150-x, y-115
     # vx, vy = dx // 100, dy // 100
     distance = math.sqrt(dx**2 + dy**2)
-    if distance > 50:
+    revving = distance <= 80
+    if distance > 80:
         nerf_turret.rev(0)
-    elif distance > 40:
-        nerf_turret.rev(50)
-    elif distance > 30:
-        nerf_turret.rev(100)
-    elif distance > 20:
-        nerf_turret.rev(150)
-    elif distance > 10:
-        nerf_turret.rev(255)
+    elif distance > 70:
+        nerf_turret.rev(40)
+    elif distance > 60:
+        nerf_turret.rev(80)
+    elif distance > 50:
+        nerf_turret.rev(120)
     else:
-        nerf_turret.rev(255)
-        nerf_turret.fire(1)
+        nerf_turret.rev(160)
+    if distance < 35:
+        time.sleep(0.1)
+        nerf_turret.fire(5)
+        time.sleep(0.2)
+        nerf_turret.rev(120)
     time.sleep(0.1)
-    # # draw the bounding box of the face along with the associated
-    # # probability
-
     key = cv2.waitKey(1) & 0xFF
 
     # if the `q` key was pressed, break from the loop
